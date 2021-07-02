82390e59e10815a19f80a4a26a1385035dc39bd5
tony <834@holbertonschool.com>
2021-07-01 22:46:34 -0500

    0x0A-slide_line

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,11 @@
+#ifndef SLIDE
+#define SLIDE
+
+#include <stdlib.h>
+
+int slide_line(int *line, size_t size, int direction);
+void swap(int *min_n, int *n);
+void sort(int *line, size_t size);
+void left(int *line, size_t size);
+
+#endif /* SLIDE_H */
